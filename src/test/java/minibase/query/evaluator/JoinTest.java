3791b2483ec62bf180183a3e71cba9c12c9380d9
andreas.baeuerle <andreas.baeuerle@uni-konstanz.de>
2021-07-03 12:55:49 +0200

    Adapted Timing on txt. Added first test.

--- src/test/java/minibase/query/evaluator/JoinTest.java
+++ src/test/java/minibase/query/evaluator/JoinTest.java
@@ -2,6 +2,7 @@ package minibase.query.evaluator;
 
 import minibase.access.file.FileScan;
 import minibase.access.file.HeapFile;
+import minibase.util.Convert;
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
@@ -18,18 +19,45 @@ public class JoinTest extends EvaluatorBaseTest {
             final FileScan sailorScan = sailors.openScan();
             final FileScan reservesScan = reserves.openScan();
 
-            // printSailor(sailorScan);
-            // printReservations(reservesScan);
+            // print the sailors
+            while (sailorScan.hasNext()) {
+                final byte[] nextTuple = sailorScan.next();
+                System.out.print(Convert.readInt(nextTuple, 0) + "\t");
+                System.out.print(Convert.readString(nextTuple, 4, 50) + "  \t");
+                System.out.print(Convert.readInt(nextTuple, 54) + "\t");
+                System.out.println(Convert.readFloat(nextTuple, 58));
+            }
 
+            // print the reserves
+            while (reservesScan.hasNext()) {
+                final byte[] nextTuple = reservesScan.next();
+                System.out.print(Convert.readInt(nextTuple, 0) + "\t");
+                System.out.print(Convert.readInt(nextTuple, 4) + "\t");
+                System.out.print(Convert.readDate(nextTuple, 8) + "\t");
+                System.out.println(Convert.readString(nextTuple, 11, 50) + "  \t");
+            }
             sailorScan.close();
             reservesScan.close();
 
             final SortMergeEquiJoin sortedMergeJoinResult = new SortMergeEquiJoin(new TableScan(S_RESERVES, reserves),
                     0, new TableScan(S_SAILORS, sailors), 0, this.getBufferManager());
             try (TupleIterator iterator = sortedMergeJoinResult.open()) {
-                //final int count = this.printReservationSMJSailors(iterator);
-                //assertEquals(count, 100);
-                //System.out.println("# matches: " + count);
+                System.out.println("hallo");
+                while (iterator.hasNext()) {
+                    final byte[] nextTuple = iterator.next();
+
+                    System.out.print(Convert.readInt(nextTuple, 0) + "\t");
+                    System.out.print(Convert.readInt(nextTuple, 4) + "  \t");
+                    System.out.print(Convert.readDate(nextTuple, 8) + "\t");
+                    System.out.print(Convert.readString(nextTuple, 11, 50) + "\t");
+
+                    System.out.print(Convert.readInt(nextTuple, 61) + "\t");
+                    System.out.print(Convert.readString(nextTuple, 65, 50) + "\t");
+                    System.out.print(Convert.readInt(nextTuple, 115) + "\t");
+                    System.out.println(Convert.readFloat(nextTuple, 119) + "  \t");
+
+                    System.out.println("\n");
+                }
             } catch (final Exception e) {
                 e.printStackTrace();
             }
